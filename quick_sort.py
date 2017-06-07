feb330f6263a01b34006752ee8f2d194c3060b2d
tamulemon <mchen@pointmarc.com>
2017-06-07 15:00:56 -0700

    quick sort

--- quick_sort.py
+++ quick_sort.py
@@ -1,4 +1,4 @@
-def quick_sort(nums):
+def quick_sort(nums, i, j):
     ''' 
     The key process in quickSort is partition(). Target of partitions is, given an array and an element x of array as pivot, 
     put x at its correct position in sorted array and put all smaller elements (smaller than x) before x, 
@@ -11,42 +11,42 @@ def quick_sort(nums):
     if len(nums) <= 1:
         return nums
 
-    #because nums[pivot] is already at permenant position, it doesn't need to be sorted again
-    pivot = sort_pivot(nums)[0]
-    nums = sort_pivot(nums)[1]
+    if i < j:
+        pivot = sort_pivot(nums, i, j)
+        left_i = i
+        left_j = pivot - 1
+        right_i = pivot + 1
+        right_j = j
 
-    left = nums[:pivot]
-    right = nums[pivot:]
+        if left_i < left_j:
+            quick_sort(nums, left_i, left_j)
+        if right_i < right_j:
+            quick_sort(nums, right_i, right_j)
+    return nums
 
-    # use the leftmost as pivot point
-    left = quick_sort(left)
-    right = quick_sort(right)
 
-    output = left.append(nums[pivot])
-    output.extend(right)
+def sort_pivot(listToSort, i, j): # i is the starting point, j is the end point
+    pivot = listToSort[i] # start with the first element as pivot value
+    left = i + 1 # move left to the next position
+    right = j
+    while left <= right: # left can not exceed right
+        if listToSort[left] <= pivot:
+            left += 1
+        if listToSort[right] >= pivot:
+            right -= 1
+        if right < left: # break immediately when left exceeds right
+            break
+        if listToSort[left] >  listToSort[right]: # swap left and right value if left value> right value
+            temp = listToSort[right]
+            listToSort[right] = listToSort[left]
+            listToSort[left] = temp
 
-    return output
+    temp = listToSort[right]        # swap pivot and right boarder, right boarder becomes the permanent pivot pivot
+    listToSort[right] = listToSort[i]
+    listToSort[i] = temp
+    return right            # return pivot positino
 
-def sort_pivot(list):
-    pivot = list[0]
-    i = 1
-    j = len(list) - 1
-    while i < j:
-        if list[i] < pivot:
-            i += 1
-        if list[j] > pivot:
-            j -= 1
-        if list[i] >  list[j]:
-            temp = list[j]
-            list[j] = list[i]
-            list[i] = temp
-            i += 1
-            j -= 1
-    temp = list[i]
-    list[i] = list[0]
-    list[0] = temp
-    return [i, list]
 
 if __name__ == '__main__':
-    nums = [6, 5, 12, 3, 0, -5, 0, -5, 2, 3, 6]
-    print quick_sort(nums)
+    nums = [6, 5, 12, -0.5, 6.8, -19, -5, 0, -5, 2, 3, 6]
+    print quick_sort(nums, 0, len(nums)-1)
